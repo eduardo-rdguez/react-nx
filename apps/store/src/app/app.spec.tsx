4b6326a78a2f3502023b07de6e21b3118b09d96a
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2022-04-17 19:43:58 -0500

    Update store app test

--- apps/store/src/app/app.spec.tsx
+++ apps/store/src/app/app.spec.tsx
@@ -1,10 +1,23 @@
-import { render } from '@testing-library/react';
+import { findByTestId, findByText, render } from '@testing-library/react';
 
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './app';
 
+function mockFetch(data: any) {
+  return jest.fn().mockImplementation(() => {
+    return Promise.resolve({
+      ok: true,
+      json: () => data,
+    });
+  });
+}
+
 describe('App', () => {
+  beforeEach(() => {
+    window.fetch = mockFetch([]);
+  });
+
   it('should render successfully', () => {
     const { baseElement } = render(
       <BrowserRouter>
@@ -12,16 +25,17 @@ describe('App', () => {
       </BrowserRouter>
     );
 
+    const element = findByTestId(baseElement, 'app-container');
     expect(baseElement).toBeTruthy();
   });
 
   it('should have a greeting as the title', () => {
-    const { getByText } = render(
+    const { baseElement } = render(
       <BrowserRouter>
         <App />
       </BrowserRouter>
     );
 
-    expect(getByText(/Welcome store/gi)).toBeTruthy();
+    expect(findByText(baseElement, 'Board Game Hoard')).toBeTruthy();
   });
 });
